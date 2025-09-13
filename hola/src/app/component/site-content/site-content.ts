f4dbc7477297823d6bafeab0e84b024644ac170c
zack0boy <gonzaacastillo3@gmail.com>
2025-09-13 19:20:00 -0300

    avances 2

---
+++ hola/src/app/component/site-content/site-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-content',
+  imports: [],
+  templateUrl: './site-content.html',
+  styleUrl: './site-content.css'
+})
+export class SiteContent {
+
+}
