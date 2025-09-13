28070ffa88aa41e3efb403266c445750e0203e91
zack0boy <gonzaacastillo3@gmail.com>
2025-09-13 19:31:17 -0300

    avances 3

--- hola/src/app/component/site-header/site-header.ts
+++ hola/src/app/component/site-header/site-header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-site-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './site-header.html',
   styleUrl: './site-header.css'
 })
